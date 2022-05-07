324454308917ad6e981fcba54ee3cfb5b0023dde
Cleonildo Jr <cleu.junior@gmail.com>
2022-05-07 15:25:58 -0300

    imperative approach

---
+++ src/main/java/com/functional/java/Main.java
@@ -0,0 +1,54 @@
+package com.functional.java;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.functional.java.Main.Gender.FEMALE;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+            new Person("John", Gender.MALE),
+            new Person("Elsa", FEMALE),
+            new Person("Lara", FEMALE),
+            new Person("Carlos", Gender.MALE)
+
+        );
+
+        // Imperative approach
+        List<Person> females = new ArrayList<>();
+
+        for(Person person: people) {
+            if (FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+
+        for(Person female: females) {
+            System.out.println(females);
+        }
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+
+        public Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        MALE, FEMALE
+    }
+}
