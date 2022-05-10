c66d514a2682514180b239ee6fcaf1bcea9d0120
Cleonildo Jr <cleu.junior@gmail.com>
2022-05-10 09:08:29 -0300

    Optionals

---
+++ src/main/java/com/functional/java/combinatorpattern/CostumerValidatorService.java
@@ -0,0 +1,8 @@
+package com.functional.java.combinatorpattern;
+
+public class CostumerValidatorService {
+
+    public boolean isEmailValid(String email) {
+        return email.contains("@");
+    }
+}
